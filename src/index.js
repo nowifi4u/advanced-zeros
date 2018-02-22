a861ca514668317d0d02fb23e640b46997f4248e
Shamber Maxim <36640388+nowifi4u@users.noreply.github.com>
2018-02-22 03:21:36 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+  let zero = number;
+  let buf = base;
+  let pow = 0;
+  let con = 0;
+  let k = 0;
+  for (let i = 2; i <= base; i++){
+    if (buf % i === 0){
+      pow = 0;
+      while (buf % i === 0){
+        pow++
+        buf = ~~(buf/i)
+      }
+      con = 0
+      k = number
+      while (k/i > 0){
+        con += ~~(k/i)
+        k = ~~(k/i)
+      }
+      zero = Math.min(zero, ~~(con/pow))
+    }
+  }
+  return zero
 }
